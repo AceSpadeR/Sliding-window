68abc104b2c50f2bf514f5c4913fbdeec2a05ab8
AceSpadeR <aspader.12@gmail.com>
2024-11-07 12:42:35 -0700

    Create

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+    string str = "";
+
+    cin >> str;
+    
+    cout << str;
+
+
+
+    return 0;
+}
